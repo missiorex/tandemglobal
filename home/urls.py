b7c14ea15c35d03aa45fae4afee1ed119f8d95eb
missiorex <support@missiorex.com>
2017-02-15 16:41:56 +0530

    Notice Details Page added + base template added

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^notice/(?P<pk>\d+)/$', views.notice_detail, name='notice_detail'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from posts.models import Notice
 from django.utils import timezone
 def index(request):
     notices = Notice.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'home/index.html', {'notices': notices})
+def notice_detail(request, pk):
+    notice = get_object_or_404(Notice, pk=pk)
+    return render(request, 'details/notice_detail.html', {'notice': notice})    
     
