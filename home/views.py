da22ebb269254d3162f3925987139f6673f8bce8
missiorex <support@missiorex.com>
2017-03-23 18:49:35 +0530

    vision-mission home page corrections

--- home/views.py
+++ home/views.py
@@ -68,45 +68,8 @@ def notice(request, pk):
     courses = Course.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     results = Result.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     notices = Notice.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    form_class = ContactForm
-    if request.method == 'POST':
-        form = form_class(data=request.POST)
-
-        if form.is_valid():
-            contact_name = request.POST.get(
-                'contact_name'
-            , '')
-            contact_email = request.POST.get(
-                'contact_email'
-            , '')
-            contact_phone = request.POST.get(
-                'contact_phone'
-            , '')
-            form_content = request.POST.get('course_details', '') 
-
-            
-            plaintemplate = get_template('contact_template.txt')
-            htmltemplate = get_template('contact_template.html')
-            context = Context({
-                'contact_name': contact_name,
-                'contact_email': contact_email,
-                'form_content': form_content,
-                'contact_phone': contact_phone,
-            })
-            textcontent = plaintemplate.render(context)
-            htmlcontent = htmltemplate.render(context)
-
-            email = EmailMultiAlternatives(
-                "New contact form submission",
-                textcontent,
-                "Tandem Global" +'',
-                ['prakash@missiorex.com'],
-                headers = {'Reply-To': contact_email }
-            )
-            email.attach_alternative(htmlcontent, "text/html")
-            email.send()
-            return redirect('notice')
-    return render(request, 'details/notice.html', {'selected_notice': selected_notice,'notices': notices,'course_categories':course_categories,'streams':streams,'courses':courses,'results':results,'form': form_class}) 
+    
+    return render(request, 'details/notice.html', {'selected_notice': selected_notice,'notices': notices,'course_categories':course_categories,'streams':streams,'courses':courses,'results':results}) 
 
 def course_detail(request, pk):
     selected_course = get_object_or_404(Course, pk=pk)
@@ -115,45 +78,8 @@ def course_detail(request, pk):
     courses = Course.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     coursedetails = CourseDetail.objects.filter(course__title=selected_course.title).order_by('order')
     results = Result.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[:5]
-    form_class = ContactForm
-    if request.method == 'POST':
-        form = form_class(data=request.POST)
-
-        if form.is_valid():
-            contact_name = request.POST.get(
-                'contact_name'
-            , '')
-            contact_email = request.POST.get(
-                'contact_email'
-            , '')
-            contact_phone = request.POST.get(
-                'contact_phone'
-            , '')
-            form_content = request.POST.get('course_details', '') 
-
-            
-            plaintemplate = get_template('contact_template.txt')
-            htmltemplate = get_template('contact_template.html')
-            context = Context({
-                'contact_name': contact_name,
-                'contact_email': contact_email,
-                'form_content': form_content,
-                'contact_phone': contact_phone,
-            })
-            textcontent = plaintemplate.render(context)
-            htmlcontent = htmltemplate.render(context)
-
-            email = EmailMultiAlternatives(
-                "New contact form submission",
-                textcontent,
-                "Tandem Global" +'',
-                ['prakash@missiorex.com'],
-                headers = {'Reply-To': contact_email }
-            )
-            email.attach_alternative(htmlcontent, "text/html")
-            email.send()
-            return redirect('course_detail')
-    return render(request, 'details/course.html', {'selected_course': selected_course,'form': form_class,'course_categories':course_categories,'streams':streams,'courses':courses,'results':results,'coursedetails':coursedetails}) 
+    
+    return render(request, 'details/course.html', {'selected_course': selected_course,'course_categories':course_categories,'streams':streams,'courses':courses,'results':results,'coursedetails':coursedetails}) 
 
 def result_detail(request, pk):
     selected_result = get_object_or_404(Result, pk=pk)
@@ -164,45 +90,8 @@ def result_detail(request, pk):
     top_scorers = TopScorer.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 	
     
-    form_class = ContactForm
-    if request.method == 'POST':
-        form = form_class(data=request.POST)
-
-        if form.is_valid():
-            contact_name = request.POST.get(
-                'contact_name'
-            , '')
-            contact_email = request.POST.get(
-                'contact_email'
-            , '')
-            contact_phone = request.POST.get(
-                'contact_phone'
-            , '')
-            form_content = request.POST.get('course_details', '') 
-
-            
-            plaintemplate = get_template('contact_template.txt')
-            htmltemplate = get_template('contact_template.html')
-            context = Context({
-                'contact_name': contact_name,
-                'contact_email': contact_email,
-                'form_content': form_content,
-                'contact_phone': contact_phone,
-            })
-            textcontent = plaintemplate.render(context)
-            htmlcontent = htmltemplate.render(context)
-
-            email = EmailMultiAlternatives(
-                "New contact form submission",
-                textcontent,
-                "Tandem Global" +'',
-                ['prakash@missiorex.com'],
-                headers = {'Reply-To': contact_email }
-            )
-            email.attach_alternative(htmlcontent, "text/html")
-            email.send()
-            return redirect('result_detail')
-    return render(request, 'details/result.html', {'selected_result': selected_result,'form': form_class,'course_categories':course_categories,'streams':streams,'courses':courses,'results':results,'top_scorers':top_scorers})
+    
+    return render(request, 'details/result.html', {'selected_result': selected_result,'course_categories':course_categories,'streams':streams,'courses':courses,'results':results,'top_scorers':top_scorers})
 
 
 def tab_detail(request):
@@ -216,45 +105,8 @@ def tab_detail(request):
     courses = Course.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     results = Result.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
   
-    form_class = ContactForm
-    if request.method == 'POST':
-        form = form_class(data=request.POST)
-
-        if form.is_valid():
-            contact_name = request.POST.get(
-                'contact_name'
-            , '')
-            contact_email = request.POST.get(
-                'contact_email'
-            , '')
-            contact_phone = request.POST.get(
-                'contact_phone'
-            , '')
-            form_content = request.POST.get('course_details', '') 
-
-            
-            plaintemplate = get_template('contact_template.txt')
-            htmltemplate = get_template('contact_template.html')
-            context = Context({
-                'contact_name': contact_name,
-                'contact_email': contact_email,
-                'form_content': form_content,
-                'contact_phone': contact_phone,
-            })
-            textcontent = plaintemplate.render(context)
-            htmlcontent = htmltemplate.render(context)
-
-            email = EmailMultiAlternatives(
-                "New contact form submission",
-                textcontent,
-                "Tandem Global" +'',
-                ['prakash@missiorex.com'],
-                headers = {'Reply-To': contact_email }
-            )
-            email.attach_alternative(htmlcontent, "text/html")
-            email.send()
-            return redirect('tab_detail')
-    return render(request, 'details/tabs.html', {'form': form_class,'newss': newss,'exams': exams,'mocks': mocks,'videos': videos,'course_categories':course_categories,'streams':streams,'courses':courses,'results':results}) 
+    
+    return render(request, 'details/tabs.html', {'newss': newss,'exams': exams,'mocks': mocks,'videos': videos,'course_categories':course_categories,'streams':streams,'courses':courses,'results':results}) 
 
 
 def contact(request):
@@ -307,6 +159,8 @@ def contact(request):
             return redirect('contact')
     return render(request, 'details/contact.html', {'form': form_class,'centers': centers,'divisions': divisions,'contacts': contacts,'course_categories':course_categories,'streams':streams,'courses':courses,'results':results})  
 
+def methodology(request):
+    return render(request, 'details/methodology.html', {})
 
 def about(request):
     
@@ -316,45 +170,8 @@ def about(request):
     courses = Course.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     results = Result.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     
-    form_class = ContactForm
-    if request.method == 'POST':
-        form = form_class(data=request.POST)
-
-        if form.is_valid():
-            contact_name = request.POST.get(
-                'contact_name'
-            , '')
-            contact_email = request.POST.get(
-                'contact_email'
-            , '')
-            contact_phone = request.POST.get(
-                'contact_phone'
-            , '')
-            form_content = request.POST.get('course_details', '') 
-
-            
-            plaintemplate = get_template('contact_template.txt')
-            htmltemplate = get_template('contact_template.html')
-            context = Context({
-                'contact_name': contact_name,
-                'contact_email': contact_email,
-                'form_content': form_content,
-                'contact_phone': contact_phone,
-            })
-            textcontent = plaintemplate.render(context)
-            htmlcontent = htmltemplate.render(context)
-
-            email = EmailMultiAlternatives(
-                "New contact form submission",
-                textcontent,
-                "Tandem Global" +'',
-                ['prakash@missiorex.com'],
-                headers = {'Reply-To': contact_email }
-            )
-            email.attach_alternative(htmlcontent, "text/html")
-            email.send()
-            return redirect('about')
-    return render(request, 'details/about.html', {'form': form_class,'slogans': slogans,'course_categories':course_categories,'streams':streams,'courses':courses,'results':results})
+    
+    return render(request, 'details/about.html', {'slogans': slogans,'course_categories':course_categories,'streams':streams,'courses':courses,'results':results})
 
 def partner(request):
     
